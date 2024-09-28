beb16fd9a808d9e17acb75d0ff24a22610f9b01c
SIGEI <andrewsigei684@gmail.com>
2024-09-28 18:22:23 +0300

    Added solutions for student grade generator, speed detector, and net salary calculator

---
+++ solutions/student-grade.js
@@ -0,0 +1,16 @@
+function getGrade(mark) {
+    if (mark > 79) {
+        return 'A';
+    } else if (mark > 69) {
+        return 'B';
+    } else if (mark > 59) {
+        return 'C';
+    } else if (mark > 49) {
+        return 'D';
+    } else {
+        return 'F';
+    }
+}
+
+const studentMark = 85; 
+console.log(`The student's grade is: ${getGrade(studentMark)}`);
